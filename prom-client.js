b708ac2ff3c2082b1f2eb5fc953bd188af1c348f
gonghuijue <gonghuijue@senseauto.com>
2024-03-06 15:08:27 +0800

    init

---
+++ prom-client.js
@@ -0,0 +1,40 @@
+const client = require("prom-client");
+
+const Registry = client.Registry;
+const register = new Registry();
+
+client.collectDefaultMetrics({ register });
+
+function useCounter({ name, help, labels }) {
+  let counter = register.getSingleMetric(name);
+  if (!counter) {
+    counter = new client.Counter({
+      name,
+      help,
+      registers: [register],
+      labelNames: Object.keys(labels),
+    });
+  }
+
+  counter.inc(labels, 1);
+}
+
+function useGauge({ name, help, labels }, value) {
+  let gauge = register.getSingleMetric(name);
+  if (!gauge) {
+    gauge = new client.Gauge({
+      name,
+      help,
+      registers: [register],
+      labelNames: Object.keys(labels),
+    });
+  }
+
+  gauge.set(labels, value);
+}
+
+module.exports = {
+  useCounter,
+  useGauge,
+  register,
+};
